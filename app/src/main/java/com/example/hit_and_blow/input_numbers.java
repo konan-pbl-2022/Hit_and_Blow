d1ff75c2baa99262644e1308ada51aeb351db602
s2171134 <mountroot238@gmail.com>
2022-11-11 17:06:13 +0900

    途中経過

---
+++ app/src/main/java/com/example/hit_and_blow/input_numbers.java
@@ -0,0 +1,30 @@
+package com.example.hit_and_blow;
+
+public class input_numbers {
+    /*
+    int digit = title.getDigit();
+    int input_nums = new int[digit];
+    Button num_1 = (Button) findviewById(R.Id.1のボタンId);
+    Button num_2 = (Button) findviewById(R.Id.2のボタンId);
+    Button num_3 = (Button) findviewById(R.Id.3のボタンId);
+    Button num_4 = (Button) findviewById(R.Id.4のボタンId);
+    Button num_5 = (Button) findviewById(R.Id.5のボタンId);
+    Button num_6 = (Button) findviewById(R.Id.6のボタンId);
+    Button num_7 = (Button) findviewById(R.Id.7のボタンId);
+    Button num_8 = (Button) findviewById(R.Id.8のボタンId);
+    Button num_9 = (Button) findviewById(R.Id.9のボタンId);
+    Button num_0 = (Button) findviewById(R.Id.0のボタンId);
+    Button seq_0 = (Button) findviewById(R.Id.配列0のボタンId);
+    Button seq_1 = (Button) findviewById(R.Id.配列1のボタンId);
+    Button seq_2 = (Button) findviewById(R.Id.配列2のボタンId);
+
+    num_1.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+
+        }
+
+    })
+
+     */
+}
