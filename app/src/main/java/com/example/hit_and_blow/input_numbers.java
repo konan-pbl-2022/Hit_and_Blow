6e68cb9f53b0cb845f0d3efa33e201c9caea01e2
s2171134 <mountroot238@gmail.com>
2022-11-11 17:22:35 +0900

    途中経過

--- app/src/main/java/com/example/hit_and_blow/input_numbers.java
+++ app/src/main/java/com/example/hit_and_blow/input_numbers.java
@@ -17,14 +17,98 @@ public class input_numbers {
     Button seq_0 = (Button) findviewById(R.Id.配列0のボタンId);
     Button seq_1 = (Button) findviewById(R.Id.配列1のボタンId);
     Button seq_2 = (Button) findviewById(R.Id.配列2のボタンId);
+    if(digit>3)Button seq_3 = (Button) findviewById(R.Id.配列3のボタンId);
+    if(digit>4)Button seq_4 = (Button) findviewById(R.Id.配列4のボタンId);
+    int change = 0;
 
     num_1.setOnClickListener(new View.OnClickListener(){
         @Override
         public void onClick(View v){
+            imput_nums[change]=1;
+            change++;
+            num_1.isEnabled=false;
+        }
+    })
+    num_2.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            imput_nums[change]=2;
+            change++;
+            num2.isEnabled=false;
+        }
+    })
+    num_3.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            imput_nums[change]=3;
+            change++;
+            num3.isEnabled=false;
+        }
+    })
+    num_4.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            imput_nums[change]=4;
+            change++;
+            num4.isEnabled=false;
+        }
+    })
+    num_5.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            imput_nums[change]=5;
+            change++;
+            num5.isEnabled=false;
+        }
+    })
+    num_6.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            imput_nums[change]=6;
+            change++;
+            num6.isEnabled=false;
+        }
+    })
+    num_7.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            imput_nums[change]=7;
+            change++;
+            num7.isEnabled=false;
+        }
+    })
+    num_8.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            imput_nums[change]=8;
+            change++;
+            num8.isEnabled=false;
+        }
+    })
+    num_9.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            imput_nums[change]=9;
+            change++;
+            num9.isEnabled=false;
+        }
+    })
+    num_0.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            imput_nums[change]=0;
+            change++;
+            num0.isEnabled=false;
+        }
+    })
+    seq_0.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
 
         }
-
     })
+
+    if(change==digit)
 
      */
 }
