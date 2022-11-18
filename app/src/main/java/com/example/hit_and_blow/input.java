b9145351f12b9aaf17542faa48b1860dd852822d
s2171134 <mountroot238@gmail.com>
2022-11-18 17:57:37 +0900

    とりあえず完成？

--- app/src/main/java/com/example/hit_and_blow/input.java
+++ app/src/main/java/com/example/hit_and_blow/input.java
@@ -8,45 +8,47 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class input {
+public class input extends AppCompatActivity {
     int change = 0;
     int[] input_nums = new int[3];
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_level1);
 
 
-        final Button ok = (Button) findviewById(R.Id.button_ok);
-        final Button num_1 = (Button) findviewById(R.Id.button1);
-        final Button num_2 = (Button) findviewById(R.Id.button2);
-        final Button num_3 = (Button) findviewById(R.Id.button3);
-        final Button num_4 = (Button) findviewById(R.Id.button4);
-        final Button num_5 = (Button) findviewById(R.Id.button5);
-        final Button num_6 = (Button) findviewById(R.Id.button6);
-        final Button num_7 = (Button) findviewById(R.Id.button7);
-        final Button num_8 = (Button) findviewById(R.Id.button8);
-        final Button num_9 = (Button) findviewById(R.Id.button9);
-        final Button num_0 = (Button) findviewById(R.Id.button0);
-        final Button delete = (Button) findviewById(R.Id.button_delete);
-        TextView seq_0 = (TextView) findViewById(R.Id.number_1);
-        TextView seq_1 = (TextView) findViewById(R.Id.number_2);
-        TextView seq_2 = (TextView) findViewById(R.Id.number_3);
-        ok.isEnabled = false;
-        num_0.isEnabled = false;
-
+        final Button ok = (Button) findViewById(R.id.button_ok);
+        final Button num_1 = (Button) findViewById(R.id.button1);
+        final Button num_2 = (Button) findViewById(R.id.button2);
+        final Button num_3 = (Button) findViewById(R.id.button3);
+        final Button num_4 = (Button) findViewById(R.id.button4);
+        final Button num_5 = (Button) findViewById(R.id.button5);
+        final Button num_6 = (Button) findViewById(R.id.button6);
+        final Button num_7 = (Button) findViewById(R.id.button7);
+        final Button num_8 = (Button) findViewById(R.id.button8);
+        final Button num_9 = (Button) findViewById(R.id.button9);
+        final Button num_0 = (Button) findViewById(R.id.button0);
+        final Button delete = (Button) findViewById(R.id.button_delete);
+        TextView seq_0 = (TextView) findViewById(R.id.number_1);
+        TextView seq_1 = (TextView) findViewById(R.id.number_2);
+        TextView seq_2 = (TextView) findViewById(R.id.number_3);
+        ok.setEnabled(false);
+        num_0.setEnabled(false);
         num_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 input_nums[change] = 1;
                 change++;
-                num_1.isEnabled = false;
+                num_1.setEnabled(false);
             }
         });
         num_2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 input_nums[change] = 2;
                 change++;
-                num_2.isEnabled = false;
+                num_2.setEnabled(false);
             }
         });
         num_3.setOnClickListener(new View.OnClickListener() {
@@ -54,7 +56,7 @@ public class input {
             public void onClick(View v) {
                 input_nums[change] = 3;
                 change++;
-                num_3.isEnabled = false;
+                num_3.setEnabled(false);
             }
         });
         num_4.setOnClickListener(new View.OnClickListener() {
@@ -62,7 +64,7 @@ public class input {
             public void onClick(View v) {
                 input_nums[change] = 4;
                 change++;
-                num_4.isEnabled = false;
+                num_4.setEnabled(false);
             }
         });
         num_5.setOnClickListener(new View.OnClickListener() {
@@ -70,7 +72,7 @@ public class input {
             public void onClick(View v) {
                 input_nums[change] = 5;
                 change++;
-                num_5.isEnabled = false;
+                num_5.setEnabled(false);
             }
         });
         num_6.setOnClickListener(new View.OnClickListener() {
@@ -78,7 +80,7 @@ public class input {
             public void onClick(View v) {
                 input_nums[change] = 6;
                 change++;
-                num_6.isEnabled = false;
+                num_6.setEnabled(false);
             }
         });
         num_7.setOnClickListener(new View.OnClickListener() {
@@ -86,7 +88,7 @@ public class input {
             public void onClick(View v) {
                 input_nums[change] = 7;
                 change++;
-                num_7.isEnabled = false;
+                num_7.setEnabled(false);
             }
         });
         num_8.setOnClickListener(new View.OnClickListener() {
@@ -94,7 +96,7 @@ public class input {
             public void onClick(View v) {
                 input_nums[change] = 8;
                 change++;
-                num_8.isEnabled = false;
+                num_8.setEnabled(false);
             }
         });
         num_9.setOnClickListener(new View.OnClickListener() {
@@ -102,7 +104,7 @@ public class input {
             public void onClick(View v) {
                 input_nums[change] = 9;
                 change++;
-                num_9.isEnabled = false;
+                num_9.setEnabled(false);
             }
         });
         num_0.setOnClickListener(new View.OnClickListener() {
@@ -110,7 +112,7 @@ public class input {
             public void onClick(View v) {
                 input_nums[change] = 0;
                 change++;
-                num_0.isEnabled = false;
+                num_0.setEnabled(false);
             }
         });
 
@@ -118,56 +120,54 @@ public class input {
             @Override
             public void onClick(View v) {
                 change = 0;
-                input_nums=new int[3];
-                num_0.isEnabled = false;
-                num_1.isEnabled = true;
-                num_2.isEnabled = true;
-                num_3.isEnabled = true;
-                num_4.isEnabled = true;
-                num_5.isEnabled = true;
-                num_6.isEnabled = true;
-                num_7.isEnabled = true;
-                num_8.isEnabled = true;
-                num_9.isEnabled = true;
-
-                ok.isEnabled = false;
+                input_nums = new int[3];
+                num_0.setEnabled(false);
+                num_1.setEnabled(true);
+                num_2.setEnabled(true);
+                num_3.setEnabled(true);
+                num_4.setEnabled(true);
+                num_5.setEnabled(true);
+                num_6.setEnabled(true);
+                num_7.setEnabled(true);
+                num_8.setEnabled(true);
+                num_9.setEnabled(true);
+                ok.setEnabled(false);
             }
         });
 
-        seq_0.setText(imput_nums[0]);
-        seq_1.setText(imput_nums[1]);
-        seq_2.setText(imput_nums[2]);
+        seq_0.setText(input_nums[0]);
+        seq_1.setText(input_nums[1]);
+        seq_2.setText(input_nums[2]);
 
-        if(change == 1){
-            num_0.isEnabled = true;
+        if (change == 1) {
+            num_0.setEnabled(true);
         }
 
-        if (change == digit) {
-            ok.isEnabled = true;
+        if (change == 3) {
+            ok.setEnabled(true);
         }
 
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                num_0.isEnabled = false;
-                num_1.isEnabled = false;
-                num_2.isEnabled = false;
-                num_3.isEnabled = false;
-                num_4.isEnabled = false;
-                num_5.isEnabled = false;
-                num_6.isEnabled = false;
-                num_7.isEnabled = false;
-                num_8.isEnabled = false;
-                num_9.isEnabled = false;
-                ok.isEnabled = false;
+                num_0.setEnabled(false);
+                num_1.setEnabled(false);
+                num_2.setEnabled(false);
+                num_3.setEnabled(false);
+                num_4.setEnabled(false);
+                num_5.setEnabled(false);
+                num_6.setEnabled(false);
+                num_7.setEnabled(false);
+                num_8.setEnabled(false);
+                num_9.setEnabled(false);
+                ok.setEnabled(false);
                 return;
             }
         });
 
-
     }
 
-    public int[] get_imput_nums(){
+    public int[] get_imput_nums () {
         return this.input_nums;
     }
 }
