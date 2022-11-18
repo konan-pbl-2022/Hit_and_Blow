e77e2da7243e2da4198ed3d7539f3f747e01bd71
s2171089 <sazitto.2022@gmail.com>
2022-11-18 13:38:39 +0900

    ゲームの根幹

--- app/src/main/java/com/example/hit_and_blow/MainActivity.java
+++ app/src/main/java/com/example/hit_and_blow/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hit_and_blow;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
