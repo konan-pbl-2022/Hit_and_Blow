31faa10019c409bd2256d8a3b37dbbbe1322096e
s2171040 <k.yusuke-1126@docomo.ne.jp>
2022-11-18 14:13:35 +0900

    ゲーム画面(ボタンID用)

---
+++ app/src/main/java/com/example/hit_and_blow/game_level1.java
@@ -0,0 +1,14 @@
+package com.example.hit_and_blow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class game_level1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_level1);
+    }
+}
