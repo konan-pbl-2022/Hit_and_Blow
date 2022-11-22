79b2a07b3091d8c2a53925b1676726e0f5a2decd
s2171012 <s2171012@s.konan-u.ac.jp>
2022-11-22 14:45:52 +0900

    ランダム決めのみ

--- app/src/main/java/com/example/hit_and_blow/game_level1.java
+++ app/src/main/java/com/example/hit_and_blow/game_level1.java
@@ -6,6 +6,28 @@ import android.os.Bundle;
 
 public class game_level1 extends AppCompatActivity {
 
+    int[] number = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    /* 配列の中身を並べ替え(シャッフル) */
+    for(int i = number.length - 1; i > 0; i--) {
+        int r = (int) (Math.random() * (i + 1));
+        int tmp = number[i];
+        number[i] = number[r];
+        number[r] = tmp;
+    }
+    //-------------------------------------------------
+    //numberからplayNumber分配列要素をコピーする
+    int playNumber = 3; //桁数決め////
+    int[] answer = new int[playNumber];
+    if(number[0] == 0) {
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = number[i + 1];
+        }
+    } else {
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = number[i];
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
