a9ec46759d0ebca609545037809928bf9a7bd679
s2171084 <nakasaka.tomoya@icloud.com>
2022-11-25 13:14:34 +0900

    result変えました

--- app/src/main/java/com/example/hit_and_blow/result.java
+++ app/src/main/java/com/example/hit_and_blow/result.java
@@ -2,7 +2,10 @@ package com.example.hit_and_blow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class result extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        Button retryButton = (Button)findViewById(R.id.button_retry);
+        retryButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(result.this,game_level1.class);
+                startActivity(intent);
+            }
+        });
+
+        Button titleButton = (Button)findViewById(R.id.button_title);
+        titleButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(result.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
